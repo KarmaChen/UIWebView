85a8876d59496ce05b2be20529eaee448d372384
KarmaChen <229192590@qq.com>
2016-06-12 22:33:11 +0800

    last

---
+++ UIWebView/TransitionVIew.h
@@ -0,0 +1,13 @@
+//
+//  TransitionVIew.h
+//  UIWebView
+//
+//  Created by Karma on 16/6/12.
+//  Copyright © 2016年 陈昆涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransitionVIew : UIView
+
+@end
---
+++ UIWebView/TransitionVIew.m
@@ -0,0 +1,21 @@
+//
+//  TransitionVIew.m
+//  UIWebView
+//
+//  Created by Karma on 16/6/12.
+//  Copyright © 2016年 陈昆涛. All rights reserved.
+//
+
+#import "TransitionVIew.h"
+
+@implementation TransitionVIew
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
